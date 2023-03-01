0638ccc9149c279cd3e8b2fdd65f065fcf929a7c
resolritter <17429390+resolritter@users.noreply.github.com>
2023-03-01 13:46:24 -0300

    remove unused code

--- app/typedefs.py
+++ app/typedefs.py
@@ -17,30 +17,6 @@ class Err(Generic[E]):
     v: E
 
 
-class Final(type):
-    @no_type_check
-    def __new__(cls, name, bases, classdict):
-        for b in bases:
-            if isinstance(b, Final):
-                raise TypeError(
-                    "type '{0}' is not an acceptable base type".format(
-                        b.__name__
-                    )
-                )
-        return type.__new__(cls, name, bases, dict(classdict))
-
-
-class Error:
-    pass
-
-
-class NetworkError(Error):
-    __metaclass__ = Final
-
-    def __init__(self, err: str):
-        pass
-
-
 @dataclass
 class Point1:
     x: int
